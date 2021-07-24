912cefa396f93c0763a7b47473504dc18a8a26dd
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-07-24 19:10:01 +0300

    added search-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
