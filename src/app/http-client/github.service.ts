2cde0acd9619ce4a9299c55ebb38a36265517b36
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-07-25 04:04:57 +0300

    added link to avatar

--- src/app/http-client/github.service.ts
+++ src/app/http-client/github.service.ts
@@ -12,26 +12,32 @@ export class GithubService {
   name:string = "ericwafula";
   
   constructor(private http:HttpClient) { 
-    this.user = new User("", "", 0);
+    this.user = new User("", "", "", 0, "");
   }
 
   getUser(){
     interface ApiResponse{
       name:string;
       login:string;
+      location: string;
       public_repos: number;
+      avatar_url: string
     }
 
     let promise = new Promise<void>((resolve, reject) => {
       this.http.get<ApiResponse>(`https://api.github.com/users/daneden`).toPromise().then(response => {
         this.user.name = response.name;
         this.user.repo = response.public_repos;
+        this.user.location = response.location;
         this.user.login = response.login;
+        this.user.avatar_url = response.avatar_url;
         resolve();
       }, error => {
         this.user.name = "Clare Limo";
         this.user.login = "clarelimo";
+        this.user.location = "Kenya"
         this.user.repo = 35;
+        this.user.avatar_url = "./assets/Avatar.png"
         reject(error);
       })
     })
--- src/app/user.ts
+++ src/app/user.ts
@@ -2,9 +2,13 @@ export class User {
     name:string;
     login:string;
     repo:number;
-    constructor(name:string, login:string, repo:number){ 
+    location: string;
+    avatar_url: string;
+    constructor(name:string, login:string, location:string, repo:number, avatar_url:string){ 
         this.name = name;
         this.repo = repo;
         this.login = login;
+        this.location = location;
+        this.avatar_url = avatar_url;
     }
 }
